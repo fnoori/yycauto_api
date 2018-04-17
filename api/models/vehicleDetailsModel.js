2ce28b1c9802500100c4c3fd32c3aece5c3ac83e
Farzam Noori <farzamn80@gmail.com>
2018-04-17 14:11:07 -0600

    fresh start

--- api/models/vehicleDetailsModel.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var vehicleDetailsSchema = new Schema({});
-
-module.exports = mongoose.model('vehicledetails', vehicleDetailsSchema);
--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'secret': 'YYCAutomotivesJWTKey',
-    'database': 'mongodb://admin:pokik68elobuK@localhost:27017/yycauto_02?authSource=admin',
-    'routes': './api/routes/apiRoutes',
-    'successfullyStartedMsg': 'YYC Automotives server started on: '
-};
