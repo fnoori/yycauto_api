7eeef023948578746103fb7bf068a3cabf9bce48
Farzam Noori <farzamn80@gmail.com>
2018-05-05 15:33:15 -0600

    beginning to add user registration

---
+++ api/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    Email: {
+        type: String,
+        required: true,
+        match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
