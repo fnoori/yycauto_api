b9e9fae0de9e27a54d298ba7bad91add622e5473
Farzam Noori <farzamn80@gmail.com>
2018-05-16 14:29:48 -0600

    Removed legacy user.js files

--- api/routes/users.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UsersController = require('../controllers/users');
-
-router.post('/signup', UsersController.signUpUser);
-
-module.exports = router;
