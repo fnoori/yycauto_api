c8b1be1a718e2f9f08ad2302a12440d7500067a3
Farzam Noori <farzamn80@gmail.com>
2018-05-18 10:54:09 -0600

    Added max restrections to schemas
    Added match to the password

--- api/models/dealership.js
+++ api/models/dealership.js
@@ -2,17 +2,22 @@ const mongoose = require('mongoose');
 
 const dealershipSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    'Name': { type: String, required: true },
-    'Phone': { type: String, required: true },
-    'Address': { type: String, required: true },
+    'Name': { type: String, required: true, max: 50 },
+    'Phone': { type: String, required: true, max: 15 },
+    'Address': { type: String, required: true, max: 50  },
     'Logo': { type: String, required: false },
     'AccountCredentials': {
         'Email': { 
             type: String,
             required: true,
-            match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            max: 50 
         },
-        'Password': { type: String, required: true },
+        'Password': { 
+            type: String,
+            required: true,
+            match: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+        },
         'AccessLevel': { type: Number, required: true }
     }
 });
