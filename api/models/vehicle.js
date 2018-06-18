d6cd587e7afc180d2ffdb00717348743b78e4630
Farzam Noori <farzamn80@gmail.com>
2018-06-18 14:22:41 -0600

    reverting

--- api/models/vehicle.js
+++ api/models/vehicle.js
@@ -36,9 +36,10 @@ const vehicleSchema = mongoose.Schema({
         Name: {type: String, required: true, max: 50},
         Email: {type: String, required: true, max: 50},
         Phone: {type: String, required: true, max: 15},
-        Address: {type: String, required: true, max: 50}
+        Address: {type: String, required: true, max: 50},
+        LogoLocation: {type: String, required: true, max: 100}
     },
-    'VehiclePhotos': {type: Array, required: true},
+    'VehiclePhotoLocation': {type: String, required: true},
     'VehicleFeatures': {type: Array, required: false},
     'AdTier': {type: String, required: true}
 });
