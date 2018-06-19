6f53508ed2e914c2ef8e5bf801c36fe7162019e4
Farzam Noori <farzamn80@gmail.com>
2018-06-19 16:31:51 -0600

    Local db when local
    When starting app using "npm run dev", localhost instance of mongodb is used

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 require('dotenv').config({path:'./config.env'});
 
+if (process.env.NODE_ENV === 'development') {
+	process.env.MONGODB_URI = 'mongodb://admin:pokik68elobuK@localhost:27017/yycauto?authSource=admin';
+}
+
 const http = require('http');
 const app = require('./app');
+
+console.log(process.env);
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
