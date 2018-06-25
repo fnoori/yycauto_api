d9a8495fd9943e9e0d923362a3d761acd20a0a19
Farzam Noori <farzamn80@gmail.com>
2018-06-25 13:48:17 -0600

    Revert "Minor update to error handling"
    This reverts commit c7c5cfa9191ad379dfb163f0cddb35fffb53a775.

--- api/utils/resMessages.js
+++ api/utils/resMessages.js
@@ -48,12 +48,14 @@ exports.INVALID_PASSWORD = [
 exports.logError = (req, res, next) => {
     var dateTime = new Date();
 
+    console.log(req);
     const error = new Error({
         _id: new mongoose.Types.ObjectId(),
         'Date': dateTime,
         'Error Message': req
     });
     error.save().then(result => {
+        console.log(result);
     });
 }
 
