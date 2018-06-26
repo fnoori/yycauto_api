de8b5aa074b3ff11add4af384604d9ca7e154b44
Farzam Noori <farzamn80@gmail.com>
2018-06-26 17:35:29 -0600

    Revert "Tested and found/fixed typos"
    This reverts commit e8cc791eb78c3bbf445c7034b972bca8817d483c.

--- api/controllers/dealerships.js
+++ api/controllers/dealerships.js
@@ -110,7 +110,7 @@ exports.signUpDealership = (req, res, next) => {
                     });
                     resMessages.nonCriticalError(resMessages.DEALERHSHIP_ALREADY_EXISTS, res);
                 } else {
-                    bcrypt.hash(creationOperations['AccountCredentials.Password'], 10, (hashErr, hash) => {
+                    bcrypt.hash(creationOperations['AccountCredentials.Passwordd'], 10, (hashErr, hash) => {
                         if (hashErr) {
                             resMessages.logError(hashErr);
                             resMessages.returnError(500, hashErr, 'bcrypt.hash()', res);
@@ -220,7 +220,7 @@ exports.loginDealership = (req, res, next) => {
                             expiresIn: '1h'
                         });
 
-                    return res.status(200).json({
+                    res.status(200).json({
                         message: resMessages.AUTHENTICATION_SUCCESS,
                         token: token
                     });
@@ -276,14 +276,7 @@ exports.updateDealership = (req, res, next) => {
             const dealershipFolder = dealership.Name.split(' ').join('_');
             // upload logo if provided
             if (req.file) {
-                googleBucketReqs.storage
-        		.bucket(googleBucketReqs.bucketName)
-        		.file('dealerships/' + dealership.Name.split(' ').join('_') + '/' + dealership.Logo).delete()
-        		.then(() => {}).catch(bucketDeleteFileErr => {
-                    resMessages.logError(bucketDeleteFileErr);
-                    resMessages.returnError(500, bucketDeleteFileErr, 'bucket.delete()', res);
-        		});
-
+                googleBucket.deleteFile('dealerships/' + dealership.Name.split(' ').join('_') + '/' + dealership.Logo);
                 updateOperations['Logo'] = 'logo.' + req.file.mimetype.split('/').pop();
             }
 
