acafe2d85bdff2086bb7d6d3f88b0fc03a650f9a
Farzam Noori <farzamn80@gmail.com>
2018-07-11 14:03:39 -0600

    Able to get dealershipId from access_token
    Updated Schema's to match the new restructuing of vehicle and dealershipId
    
    Fixed emptyDir function used to empty the tmp dir
    
    Added more env variables

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -13,12 +13,12 @@ module.exports = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://yyc-automotives.auth0.com/.well-known/jwks.json`
+    jwksUri: process.env.JWKSURI
   }),
 
   // Validate the audience and the issuer.
-  audience: 'https://yyc-automotives-auth0-api',
-  issuer: `https://yyc-automotives.auth0.com/`,
+  audience: process.env.AUDIENCE,
+  issuer: process.env.ISSUER,
   algorithms: ['RS256']
 })
 
--- api/models/dealership.js
+++ api/models/dealership.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const dealershipSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    'Name': { type: String, required: true, max: 50 },
-    'Phone': { type: String, required: true, max: 15 },
-    'Address': { type: String, required: true, max: 50  },
-    'AccessLevel': { type: Number, required: true }
+    'name': { type: String, required: true, max: 50 },
+    'email': { type: String, required: true, max: 100 },
+    'phone': { type: String, required: true, max: 15 },
+    'address': { type: String, required: true, max: 50  },
+    'permission': { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Dealership', dealershipSchema);
