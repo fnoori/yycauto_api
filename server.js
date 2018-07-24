cd2ef06779b739dfc6a92cb03fa45555cee502f3
Farzam Noori <farzamn80@gmail.com>
2018-07-24 11:53:05 -0600

    Updated user/password for mlab

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config({path:'./config.env'});
 
 if (process.env.NODE_ENV === 'development') {
-	process.env.MONGODB_URI = 'mongodb://admin:pokik68elobuK@localhost:27017/yycauto?authSource=admin';
+	//process.env.MONGODB_URI = 'mongodb://admin:pokik68elobuK@localhost:27017/yycauto?authSource=admin';
 }
 
 const http = require('http');
