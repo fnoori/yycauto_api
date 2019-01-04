b73ca7af9229961e981a1733e9424a42b75627ce
Farzam Noori <fnoori431@gmail.com>
2019-01-04 15:03:15 -0700

    updating everything but the hours is working correctly
    may need to user Passportjs to authenticate via api, instead of a frontend from auth0

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const _ = require('underscore');
 
 const UserModel = require('../models/user');
 const ErrorModel = require('../models/error');
 const mongoSanitize = require('mongo-sanitize');
 
 const errorUtils = require('../utils/errorUtils');
-const excludedParams = '-_id -__v';
+const excludedParams = '-_id -__v -auth0_id';
 
 exports.get_all_dealerships = (req, res, next) => {
   UserModel.find()
@@ -25,6 +26,7 @@ exports.get_dealership_by_id = (req, res, next) => {
   }
 
   UserModel.findById(req.body.id)
+  .select(excludedParams)
   .then(user => {
     res.status(201).json(user);
   }).catch(findErr => {
@@ -33,12 +35,18 @@ exports.get_dealership_by_id = (req, res, next) => {
   });
 }
 
+// users are only allowed to update: phone, address, and dealership name from this API
 exports.update_dealership = (req, res, next) => {
   const preCheck_phone = mongoSanitize(req.body.phone);
   const preCheck_address = mongoSanitize(req.body.address);
   const preCheck_dealershipName = mongoSanitize(req.body.dealership_name);
-  var updateData = {}
+  var auth0Id = '';
+  var userId = '';
+  var updateData = {};
 
+  if (!validator.isMongoId(req.body.id)) {
+    return res.status(400).json(errorUtils.error_message('Incorrect id format', 400));
+  }
   if (preCheck_phone !== undefined && !validator.isMobilePhone(preCheck_phone)) {
     return res.status(400).json(errorUtils.error_message('Incorrect phone number format', 400));
   }
@@ -54,11 +62,21 @@ exports.update_dealership = (req, res, next) => {
     updateData.dealership_name = preCheck_dealershipName;
   }
 
-  UserModel.findOne({ auth0_id: req.body.auth0_id })
+  // check to make sure at least one thing is being updated
+  if (_.isEmpty(updateData)) {
+    return res.status(400).json(errorUtils.error_message('At least one field must be provided to update', 400));
+  }
+
+  // assign auth0_id and id
+  auth0Id = req.body.auth0_id;
+  userId = req.body.id;
+
+  // after sanitizing data and checking for existance of data, begin update process
+  UserModel.findOne({ auth0_id: auth0Id })
   .exec()
   .then(user => {
     if (user) {
-      if (req.body.id === String(user._id)) {
+      if (userId === String(user._id)) {
 
         // user found, now update
         UserModel.updateOne({ _id: user._id }, updateData)
@@ -67,7 +85,7 @@ exports.update_dealership = (req, res, next) => {
           res.status(201).json({message: 'Successfully updated'});
         }).catch(updateErr => {
           errorUtils.storeError(500, updateErr);
-          return res.status(500).json(errorUtils.error_message('mongoose.update() failed', 500));
+          return res.status(500).json(errorUtils.error_message('mongoose.updateOne() failed', 500));
         });
 
       } else {
@@ -78,10 +96,92 @@ exports.update_dealership = (req, res, next) => {
     }
   }).catch(findErr => {
     errorUtils.storeError(500, findErr);
-    return res.status(500).json(errorUtils.error_message('mongoose.find() failed', 500));
+    return res.status(500).json(errorUtils.error_message('mongoose.findOne() failed', 500));
   });
 }
 
+exports.update_dealership_hours = (req, res, next) => {
+  var auth0Id = '';
+  var id = '';
+  const sunday = _.isUndefined(mongoSanitize(req.body.sundayHours)) ? '' : mongoSanitize(req.body.sundayHours);
+  const monday = _.isUndefined(mongoSanitize(req.body.mondayHours)) ? '' : mongoSanitize(req.body.mondayHours);
+  const tuesday = _.isUndefined(mongoSanitize(req.body.teusdayHours)) ? '' : mongoSanitize(req.body.teusdayHours);
+  const wednesday = _.isUndefined(mongoSanitize(req.body.wednesdayHours)) ? '' : mongoSanitize(req.body.wednesdayHours);
+  const thursday = _.isUndefined(mongoSanitize(req.body.thursdayHours)) ? '' : mongoSanitize(req.body.thursdayHours);
+  const friday = _.isUndefined(mongoSanitize(req.body.fridayHours)) ? '' : mongoSanitize(req.body.fridayHours);
+  const saturday = _.isUndefined(mongoSanitize(req.body.saterdayHours)) ? '' : mongoSanitize(req.body.saterdayHours);
+  var updateData = {};
+
+  if (sunday.length > 0) {
+    updateData['dealership_hours.Sunday.from'] = sunday.split(' ')[0];
+    updateData['dealership_hours.Sunday.to'] = sunday.split(' ')[1];
+  }
+  if (monday.length > 0) {
+    updateData['dealership_hours.Monday.from'] = monday.split(' ')[0];
+    updateData['dealership_hours.Monday.to'] = monday.split(' ')[1];
+  }
+  if (tuesday.length > 0) {
+    updateData['dealership_hours.Tuesday.from'] = tuesday.split(' ')[0];
+    updateData['dealership_hours.Tuesday.to'] = tuesday.split(' ')[1];
+  }
+  if (wednesday.length > 0) {
+    updateData['dealership_hours.Wednesday.from'] = wednesday.split(' ')[0];
+    updateData['dealership_hours.Wednesday.to'] = wednesday.split(' ')[1];
+  }
+  if (thursday.length > 0) {
+    updateData['dealership_hours.Thursday.from'] = thursday.split(' ')[0];
+    updateData['dealership_hours.Thursday.to'] = thursday.split(' ')[1];
+  }
+  if (friday.length > 0) {
+    updateData['dealership_hours.Friday.from'] = friday.split(' ')[0];
+    updateData['dealership_hours.Friday.to'] = friday.split(' ')[1];
+  }
+  if (saturday.length > 0) {
+    updateData['dealership_hours.Saturday.from'] = saturday.split(' ')[0];
+    updateData['dealership_hours.Saturday.to'] = saturday.split(' ')[1];
+  }
+
+  // assign auth0_id and id
+  auth0Id = req.body.auth0_id;
+  userId = req.body.id;
+
+  // after sanitizing data and checking for existance of data, begin update process
+  UserModel.findOne({ auth0_id: auth0Id })
+  .exec()
+  .then(user => {
+    if (user) {
+      if (userId === String(user._id)) {
+
+        // user found, now update
+        UserModel.updateOne({ _id: user._id }, updateData)
+        .exec()
+        .then(update =>  {
+          res.status(201).json({message: 'Successfully updated'});
+        }).catch(updateErr => {
+          errorUtils.storeError(500, updateErr);
+          return res.status(500).json(errorUtils.error_message('mongoose.updateOne() failed', 500));
+        });
+
+      } else {
+        return res.status(401).json(errorUtils.error_message('Unauthorized access', 401));
+      }
+    } else {
+      return res.status(404).json(errorUtils.error_message('User doesn\'t exist', 404));
+    }
+
+
+  }).catch(findOneErr => {
+    errorUtils.storeError(500, findErr);
+    return res.status(500).json(errorUtils.error_message('mongoose.findOne() failed', 500));
+  });
+
+
+
+
+//var userType = userIsYoungerThan18 ? "Minor" : "Adult";
+
+
+}
 
 // the following method is created for testing purposes
 exports.get_my_dealership = (req, res, next) => {
