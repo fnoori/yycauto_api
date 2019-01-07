2eedc2cef013439d6ce4aada15e0a9e29c7962e6
Farzam Noori <fnoori431@gmail.com>
2019-01-07 12:31:46 -0700

    Trying to understand mocha/chai
    putting off testing until later

--- test/test.js
+++ test/test.js
@@ -1,4 +1,18 @@
-var assert = require('assert');
+const HOUR_LENGTH = 4;
+const MIN_LENGTH = 0;
+const FROM = 0;
+const TO = 1;
+const SUNDAY = 'Sunday';
+const MONDAY = 'Monday';
+const TUESDAY = 'Tuesday';
+const WEDNESDAY = 'Wednesday';
+const THURSDAY = 'Thursday';
+const FRIDAY = 'Friday';
+const SATURDAY = 'Saturday';
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const should = require('should');
 const validator = require('validator');
 const _ = require('underscore');
 const mongoSanitize = require('mongo-sanitize');
@@ -16,21 +30,24 @@ describe('Array', function() {
 */
 
 describe('Users', () => {
+  var sundayHours = '0900-1600';
+  var mondayHours = '0900-1600';
+  var tuesdayHours = '0900-1600';
+  var wednesdayHours = '0900-1600';
+  var thursdayHours = '0900-1600';
+  var fridayHours = '0900-1600';
+  var saturdayHours = '0900-1600';
+  var id = '5c2fd67317b29c00070795ab';
+  var updateData = {};
+  var shouldPass = false;
+
     describe('#update_dealership_hours()', () => {
+      shouldPass = true;
 
-      const sundayHours = '0900-1600';
-      const mondayHours = '0900-1600';
-      const tuesdayHours = '0900-1600';
-      const wednesdayHours = '0900-1600';
-      const thursdayHours = '0900-1600';
-      const fridayHours = '0900-1600';
-      const saturdayHours = '0900-1600';
-      const id = '';
+      it('should successfully execute, all inputs are perfect', () => {
 
-      it('should pass, when all correctly formatted arguments have been provided', () => {
-
-        if (!validator.isMongoId(req.body.id)) {
-          return res.status(400).json(errorUtils.error_message('Incorrect id format', 400));
+        if (!validator.isMongoId(id)) {
+          assert.ok(false);
         }
 
         const sunday = _.isUndefined(mongoSanitize(sundayHours)) ? { day: SUNDAY } : { day: SUNDAY, hours: mongoSanitize(sundayHours) };
@@ -50,11 +67,11 @@ describe('Users', () => {
 
             if (!utils.isLengthExact(from, HOUR_LENGTH) ||
                 !utils.isLengthExact(to, HOUR_LENGTH)) {
-                  return res.status(400).json(errorUtils.error_message('Must use 24 hour time format', 400));
+                  assert.ok(false, 'Must use 24 hour time format');
                 }
             if (!validator.isInt(from) ||
                 !validator.isInt(to)) {
-                  return res.status(400).json(errorUtils.error_message('Times must be numbers', 400));
+                  assert.ok(false, 'Times must be numbers');
                 }
 
             updateData['dealership_hours.' + days[day].day + '.from'] = from;
@@ -62,6 +79,58 @@ describe('Users', () => {
           }
         }
 
+        assert.ok(shouldPass);
+
     });
+
+    it('should successfully catch that an hour is too short', () => {
+      sundayHours = '0900-1600';
+      mondayHours = '0900-160';
+      tuesdayHours = '0900-1600';
+      wednesdayHours = '0900-1600';
+      thursdayHours = '0900-1600';
+      fridayHours = '0900-1600';
+      saturdayHours = '0900-1600';
+      id = '5c2fd67317b29c00070795ab';
+      var updateData = {};
+
+
+      if (!validator.isMongoId(id)) {
+        assert.ok(false);
+      }
+
+      const sunday = _.isUndefined(mongoSanitize(sundayHours)) ? { day: SUNDAY } : { day: SUNDAY, hours: mongoSanitize(sundayHours) };
+      const monday = _.isUndefined(mongoSanitize(mondayHours)) ? { day: MONDAY } : { day: MONDAY, hours: mongoSanitize(mondayHours) };
+      const tuesday = _.isUndefined(mongoSanitize(tuesdayHours)) ? { day: TUESDAY } : { day: TUESDAY, hours: mongoSanitize(tuesdayHours) };
+      const wednesday = _.isUndefined(mongoSanitize(wednesdayHours)) ? { day: WEDNESDAY } : { day: WEDNESDAY, hours: mongoSanitize(wednesdayHours) };
+      const thursday = _.isUndefined(mongoSanitize(thursdayHours)) ? { day: THURSDAY } : { day: THURSDAY, hours: mongoSanitize(thursdayHours) };
+      const friday = _.isUndefined(mongoSanitize(fridayHours)) ? { day: FRIDAY } : { day: FRIDAY, hours: mongoSanitize(fridayHours) };
+      const saturday = _.isUndefined(mongoSanitize(saturdayHours)) ? { day: SATURDAY } : { day: SATURDAY, hours: mongoSanitize(saturdayHours) };
+      const days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday];
+
+      for (day in days) {
+        // only if times are being changed for the day
+        if (!_.isUndefined(days[day].hours)) {
+          var from = days[day].hours.split('-')[0];
+          var to = days[day].hours.split('-')[1];
+
+          if (!utils.isLengthExact(from, HOUR_LENGTH) ||
+              !utils.isLengthExact(to, HOUR_LENGTH)) {
+                assert.isNotOk(false, `incorrect format in: ${from} or ${to}`);
+              }
+          if (!validator.isInt(from) ||
+              !validator.isInt(to)) {
+                assert.isNotOk(false, 'Times must be numbers');
+              }
+
+          updateData['dealership_hours.' + days[day].day + '.from'] = from;
+          updateData['dealership_hours.' + days[day].day + '.to'] = to;
+        }
+      }
+
+      assert.isTrue(true);
+
+    });
+
   });
 });
